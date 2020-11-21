b7529f35d6c7861b06e774b5c1a0730872a4ac2e
fernancumez <fernancumez@gmail.com>
2020-11-21 12:07:53 -0600

    Importando el Componente de Modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,7 +1,45 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
 import { ModalContext } from "../context/ModalContext";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Receta = ({ receta }) => {
+  // Configuracion del modal de Material UI
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Extraer los valores del context
   const { guardarIdReceta } = useContext(ModalContext);
 
   return (
@@ -17,10 +55,24 @@ const Receta = ({ receta }) => {
           <button
             type="button"
             className="btn block btn-primary"
-            onClick={() => guardarIdReceta(receta.idDrink)}
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
+            }}
           >
             Ver Receta
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              guardarIdReceta(null);
+              handleClose();
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h1>Desde Modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
